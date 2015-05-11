5954e8382a2abfbf889b76dcac119a9f8207bf1f
Rakhat Jabagin <neilhem@hotmail.com>
2015-05-11 22:12:34 +0600

    bug fixes

--- app/scripts/canvas.js
+++ app/scripts/canvas.js
@@ -10,44 +10,77 @@
   var canvasPattern = new fabric.Canvas('pattern');
   var canvasImage = new fabric.StaticCanvas('showImage');
   var defaultPattern = true;
-  canvasPattern.backgroundColor = '#fff';
+  canvasPattern.backgroundColor = 'rgba(0,0,0,0)';
+
+  var imageConfig = {
+    top: 100,
+    left: 100,
+    borderColor: '#bfbfbf',
+    cornerColor: '#bfbfbf',
+    cornerSize: 6,
+    lockUniScaling: true
+  };
+
+  console.log(fabric);
+
+  function renderImage() {
+    var img = new fabric.Image(document.getElementById('pattern'), {
+      top: 0,
+      left: 0
+    });
+
+    var patternSourceCanvas = new fabric.StaticCanvas();
+    patternSourceCanvas.add(img);
+
+    var pattern = new fabric.Pattern({
+      source: function() {
+        patternSourceCanvas.setDimensions({
+          width: img.getWidth(),
+          height: img.getHeight()
+        });
+        return patternSourceCanvas.getElement();
+      },
+      repeat: 'repeat'
+    });
+
+    canvasImage.add(new fabric.Polygon([
+      {x: 0, y: 0},
+      {x: canvasImage.width, y: 0},
+      {x: canvasImage.width, y: canvasImage.height},
+      {x: 0, y: canvasImage.height}
+    ], {
+      left: 0,
+      top: 0,
+      fill: pattern
+    }));
+  }
 
   fabric.Image.fromURL('images/forest/pattern.png', function(img) {
     img.scaleToWidth(265);
     canvasPattern.add(img).setActiveObject(img);
 
     canvasPattern.renderAll();
+    renderImage();
   });
 
-  fabric.Image.fromURL($('#showImage').data('image'), function(img) {
-    console.log(img.width, img.height, canvasImage);
-    canvasImage.width = img.width;
-    canvasImage.height = img.height;
-    canvasImage.add(img);
-    canvasImage.renderAll();
+  canvasPattern.on('mouse:up', function(options) {
+    console.log(options.e.clientX, options.e.clientY);
+    renderImage();
   });
 
   function addPattern(image) {
-    console.log(image);
+
+    // remove default pattern on first call
     if (defaultPattern) {
       defaultPattern = false;
-      console.log(canvasPattern);
+      canvasPattern.remove(canvasPattern.getActiveObject());
     }
 
     var imgElement = image;
-    var imgInstance = new fabric.Image(imgElement, {
-      top: 100,
-      left: 100,
-      borderColor: '#bfbfbf',
-      cornerColor: '#bfbfbf',
-      cornerSize: 6,
-      lockScalingFlip: true
-    });
+    var imgInstance = new fabric.Image(imgElement, imageConfig);
     imgInstance.scaleToWidth(70);
     canvasPattern.add(imgInstance);
   }
-
-  // canvasPattern.setOverlayImage(canvasImage, canvasPattern.renderAll.bind(canvasPattern));
 
   $(function() {
 
@@ -64,6 +97,7 @@
     // add selected pattern to pattern canvas
     $('.pattern-list > li > img').on('click', function() {
       addPattern(this);
+      renderImage(canvasPattern.toDataURL());
     });
   });
 
