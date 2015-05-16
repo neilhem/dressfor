e60b64f8caa01694b3733cf616d42247e1a2468a
Rakhat Jabagin <neilhem@hotmail.com>
2015-05-16 23:58:45 +0600

    update create view

--- app/scripts/canvas.js
+++ app/scripts/canvas.js
@@ -1,105 +1,125 @@
-/* global fabric, patternConfig */
+/* global fabric */
 
-(function($, config) {
+(function($, fabric) {
   'use strict';
 
   if (!$('#showImage').length) {
     return;
   }
 
-  var canvasPattern = new fabric.Canvas('pattern');
-  var canvasImage = new fabric.StaticCanvas('showImage');
-  var defaultPattern = true;
-  canvasPattern.backgroundColor = 'rgba(0,0,0,0)';
+  $.getJSON('patterns.json', function(data) {
+    var config = data;
 
-  var imageConfig = {
-    top: 100,
-    left: 100,
-    borderColor: '#bfbfbf',
-    cornerColor: '#bfbfbf',
-    cornerSize: 6,
-    lockUniScaling: true
-  };
+    var catIndex = $('.pattern').data('index');
 
-  console.log(fabric);
-
-  function renderImage() {
-    var img = new fabric.Image(document.getElementById('pattern'), {
-      top: 0,
-      left: 0
-    });
-
-    var patternSourceCanvas = new fabric.StaticCanvas();
-    patternSourceCanvas.add(img);
-
-    var pattern = new fabric.Pattern({
-      source: function() {
-        patternSourceCanvas.setDimensions({
-          width: img.getWidth(),
-          height: img.getHeight()
-        });
-        return patternSourceCanvas.getElement();
-      },
-      repeat: 'repeat'
-    });
-
-    canvasImage.add(new fabric.Polygon([
-      {x: 0, y: 0},
-      {x: canvasImage.width, y: 0},
-      {x: canvasImage.width, y: canvasImage.height},
-      {x: 0, y: canvasImage.height}
-    ], {
-      left: 0,
-      top: 0,
-      fill: pattern
-    }));
-  }
-
-  fabric.Image.fromURL('images/forest/pattern.png', function(img) {
-    img.scaleToWidth(265);
-    canvasPattern.add(img).setActiveObject(img);
-
-    canvasPattern.renderAll();
-    renderImage();
-  });
-
-  canvasPattern.on('mouse:up', function(options) {
-    console.log(options.e.clientX, options.e.clientY);
-    renderImage();
-  });
-
-  function addPattern(image) {
-
-    // remove default pattern on first call
-    if (defaultPattern) {
-      defaultPattern = false;
-      canvasPattern.remove(canvasPattern.getActiveObject());
-    }
-
-    var imgElement = image;
-    var imgInstance = new fabric.Image(imgElement, imageConfig);
-    imgInstance.scaleToWidth(70);
-    canvasPattern.add(imgInstance);
-  }
-
-  $(function() {
-
-    // create patterns elements
-    var catIndex = $('.pattern-list').data('index');
     var catName = config.categories[catIndex].name;
     var catPatterns = config.categories[catIndex].patterns;
-    $.each(catPatterns, function(i, pattern) {
-      $('.pattern-list').append(
-        '<li><img src="images/' + catName + '/patterns/' + pattern.colors[0].image + '.png" alt="">'
-      );
+
+    var canvasPattern = new fabric.Canvas('pattern');
+    var canvasImage = new fabric.StaticCanvas('showImage');
+    var defaultPattern = true;
+    canvasPattern.backgroundColor = 'rgba(0,0,0,0)';
+
+    var imageConfig = {
+      top: 100,
+      left: 100,
+      borderColor: '#d2d2d2',
+      cornerColor: '#d2d2d2',
+      cornerSize: 5,
+      lockUniScaling: true
+    };
+
+    console.log(fabric);
+
+    function renderImage() {
+      var img = new fabric.Image(document.getElementById('pattern'), {
+        top: 0,
+        left: 0
+      });
+
+      var patternSourceCanvas = new fabric.StaticCanvas();
+      patternSourceCanvas.add(img);
+
+      var pattern = new fabric.Pattern({
+        source: function() {
+          patternSourceCanvas.setDimensions({
+            width: img.getWidth(),
+            height: img.getHeight()
+          });
+          return patternSourceCanvas.getElement();
+        },
+        repeat: 'repeat'
+      });
+
+      canvasImage.add(new fabric.Polygon([
+        {x: 0, y: 0},
+        {x: canvasImage.width, y: 0},
+        {x: canvasImage.width, y: canvasImage.height},
+        {x: 0, y: canvasImage.height}
+      ], {
+        left: 0,
+        top: 0,
+        fill: pattern
+      }));
+    }
+
+    fabric.Image.fromURL('images/' + catName + '/pattern.png', function(img) {
+      img.scaleToWidth(265);
+      canvasPattern.add(img);
+
+      canvasPattern.renderAll();
+      renderImage();
     });
 
-    // add selected pattern to pattern canvas
-    $('.pattern-list > li > img').on('click', function() {
-      addPattern(this);
-      renderImage(canvasPattern.toDataURL());
+    canvasPattern.on('mouse:up', function(options) {
+      renderImage();
+    });
+
+    canvasPattern.on('mouse:out', function(options) {
+      console.log(options);
+    });
+
+    function addPattern(image) {
+
+      // remove default pattern on first call
+      if (defaultPattern) {
+        defaultPattern = false;
+        canvasPattern.remove(canvasPattern.getActiveObject());
+      }
+
+      var imgElement = image;
+      var imgInstance = new fabric.Image(imgElement, imageConfig);
+      imgInstance.scaleToWidth(70);
+      canvasPattern.add(imgInstance);
+      console.log(canvasPattern.item());
+    }
+
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    function randomColor(colors) {
+      return getRandomInt(0, colors.length);
+    }
+
+    $(function() {
+      // create patterns elements
+      $.each(catPatterns, function(i, pattern) {
+
+        var randomColorIndex = pattern.colors.length > 1 ? randomColor(pattern.colors) : 0;
+
+        $('.pattern-list .list-unstyled').append(
+          '<li><img src="images/' + catName + '/patterns/' + pattern.colors[randomColorIndex].image +
+            '.png" title="Add pattern" alt=""></li>'
+        );
+      });
+
+      // add selected pattern to pattern canvas
+      $('.pattern-list ul > li > img').on('click', function() {
+        console.log(this);
+        addPattern(this);
+        renderImage(canvasPattern.toDataURL());
+      });
     });
   });
-
-})(jQuery, patternConfig);
-
+})(jQuery, fabric);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,190 +1,16 @@
 // jshint devel:true
 'use strict';
 
-var patternConfig = {
-  'categories': [
-    {
-      'name': 'forest',
-      'patterns': [
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '01'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'red',
-              'image': '02_1'
-            },
-            {
-              'name': 'yellow',
-              'image': '02_2'
-            },
-            {
-              'name': 'green',
-              'image': '02_3'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '03'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '04'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '05'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'yellow',
-              'image': '06_2'
-            },
-            {
-              'name': 'green',
-              'image': '06_3'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '07'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'red',
-              'image': '08_1'
-            },
-            {
-              'name': 'yellow',
-              'image': '08_2'
-            },
-            {
-              'name': 'green',
-              'image': '08_3'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '09'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'yellow',
-              'image': '10_2'
-            },
-            {
-              'name': 'green',
-              'image': '10_3'
-            },
-            {
-              'name': 'black',
-              'image': '10_4'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '11'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '12_1'
-            },
-            {
-              'name': 'yellow',
-              'image': '12_2'
-            },
-            {
-              'name': 'green',
-              'image': '12_3'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'yellow',
-              'image': '13_2'
-            },
-            {
-              'name': 'green',
-              'image': '13_3'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '14'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '15'
-            }
-          ]
-        },
-        {
-          'colors': [
-            {
-              'name': 'black',
-              'image': '16'
-            }
-          ]
-        },
-      ]
-    }
-  ]
-};
-
-var userSelectedCat = false;
-
 $(function() {
+
   $('.js-raty').raty({
     starOff: '/images/star-off.png',
     starOn: '/images/star-on.png'
+  });
+
+  $('.js-scrollbar').mCustomScrollbar({
+    axis: 'y',
+    theme: 'custom'
   });
 
   var swiper = new Swiper('.swiper-container', {
@@ -192,15 +18,10 @@ $(function() {
     paginationClickable: '.swiper-pagination',
     autoplay: 5000,
     spaceBetween: 30,
-    effect: 'fade',
-    onSlideChangeEnd: function(swiper) {
-      if (!userSelectedCat) {
-        // console.log(swiper);
-      }
-    }
+    effect: 'fade'
   });
 
-  $('.js-cat .dropdown-menu > li > a').on('click', function(e) {
+  $(document).on('click', '.js-cat .dropdown-menu > li > a', function(e) {
     e.preventDefault();
     console.log(e);
 
@@ -212,12 +33,3 @@ $(function() {
     $('.js-cat-link').attr('href', $(this).attr('href'));
   });
 });
-
-// if ($('#showImage').length) {
-//   // Show canvas
-//   var show = {};
-//   show.canvas = document.getElementById('showImage');
-//   show.context = show.canvas.getContext('2d');
-//   show.context.fillStyle = '#fbfbfb';
-//   show.context.fillRect(0, 0, show.canvas.width, show.canvas.height);
-// }
